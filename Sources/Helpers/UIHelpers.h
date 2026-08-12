28c04f6c360ed4fa4b2645e1ebd4ff50b5dfe0de
bill103917-dev <bill103917@gmail.com>
2026-08-13 00:04:46 +0800

    Create UIHelpers.h

---
+++ Sources/Helpers/UIHelpers.h
@@ -0,0 +1,9 @@
+#import <UIKit/UIKit.h>
+
+UIColor *AppBackgroundColor(void);
+
+UIButton *MakeButton(NSString *title, UIColor *color);
+
+UILabel *MakeLabel(NSString *text,
+                   CGFloat size,
+                   UIFontWeight weight);
